198bf01efcfa8f4ca80242c128437ea12f246139
pedrodiasparente <pdparente@gmail.com>
2021-01-21 17:51:14 +0000

    routes

--- app-server/routes/index.js
+++ app-server/routes/index.js
@@ -2,8 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', verificaAutenticacao, function(req, res, next) {
+  res.render('index');
 });
+
+function verificaAutenticacao(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  }
+  else {
+    res.redirect("/users/login");
+  }
+}
+
+function verificaAcessoAdmin(req, res, next) {
+  if (req.user.access == 'ADMIN') {
+    next();
+  }
+  else {
+    res.redirect("/users/register");//badalhoco
+  }
+}
 
 module.exports = router;
--- app-server/routes/users.js
+++ app-server/routes/users.js
@@ -1,9 +1,61 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var User = require('../controllers/user')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function (req, res) {
+    res.render('login')
+})
+
+router.get('/perfil', function(req, res){
+    res.render('perfil', {dados: req.user})
+})
+
+router.get('/perfil/editar', function(req, res){
+    res.render('editarPerfil',{dados: req.user})
+})
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    req.session.destroy(function (err) {
+        if (!err) {
+            res.redirect('/');
+        } else {
+            console.log('Destroy session error: ', err)
+        }
+    });
+})
+
+router.get('/register', function (req, res) {
+    res.render('reg-form');
 });
+
+/*router.get('/:id', function (req, res) {
+    User.lookUp(req.params.id)
+        .then(dados => res.status(200).jsonp(dados))
+        .catch(err => res.status(500).jsonp(err))
+})*/
+
+router.post('/login', passport.authenticate('local'), function (req, res) {
+    req.user.dataUltimoAcesso = new Date().toISOString().substr(0,19)
+    //TENHO QUE DAR LOOKUP DO USER, MUDAR A DATA E DEPOIS EDIT, PORQUE O REQ.USER NAO VAI TER A INFO TODA
+    //USAR JWT
+    User.edit(req.user._id, req.user)
+        .then(dados => res.redirect('/'))
+        .catch(err => console.log(err))
+})
+
+router.post('/register', function (req, res) {
+    req.body.dataRegisto = new Date().toISOString().substr(0,19)
+    User.insert(req.body)
+        .then(dados => res.redirect('/'))
+        .catch(error => res.render('error', { error: error }))
+})
+
+/* editar perfil
+router.post('/users/perfil', function(req, res){
+
+})
+*/
 
 module.exports = router;
