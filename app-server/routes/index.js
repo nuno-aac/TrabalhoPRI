5b6e53354ac5d18dcb82bb2a60d830cabf0e5dd0
CoelhoLado <naac.1999@gmail.com>
2021-01-22 13:09:37 +0000

    nada

--- app-server/routes/index.js
+++ app-server/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   axios.get('http://localhost:6969/').then(dados => {
-    res.render('index');
+    console.log(dados)
+    //res.render('index');
   })
 });
 
