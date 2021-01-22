8277d675d9df1ceebee82f92d8bb22147d6ad113
CoelhoLado <naac.1999@gmail.com>
2021-01-22 18:26:56 +0000

    still not fixed

--- app-server/routes/index.js
+++ app-server/routes/index.js
@@ -4,15 +4,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  axios.get('http://localhost:6969/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im51bm8iLCJsZXZlbCI6IlVTRVIiLCJpYXQiOjE2MTEzMjQ5NzEsImV4cCI6MTYxMTMzNTc3MX0.brHCcztpYB6p9n2xLf4XJ0BqTzf9ICLJjWfNy9vNTvc').then(dados => {
-    console.log(dados)
+  axios.get('http://localhost:6969/').then(dados => {
+    console.log(dados.data)
     if(dados.status == 201)
       res.render('index');
-    else if (dados.status = 401)
+    else if (dados.status == 401)
       res.redirect('/user/login')
     else
       res.render('httperror',{error: dados.data})
-  }).catch( err => {res.render('error',{error:err}); console.log(err)})
+  }).catch( err => {res.render('error',{error:err});})
 });
 
 module.exports = router;
--- app-server/routes/users.js
+++ app-server/routes/users.js
@@ -15,7 +15,7 @@ router.get('/perfil/editar', function(req, res){
 })
 
 router.get('/logout', function (req, res) {
-    axios.get('http://localhost:6969/users/logout?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im51bm8iLCJsZXZlbCI6IlVTRVIiLCJpYXQiOjE2MTEzMjQ5NzEsImV4cCI6MTYxMTMzNTc3MX0.brHCcztpYB6p9n2xLf4XJ0BqTzf9ICLJjWfNy9vNTvc').then(dados => {
+    axios.get('http://localhost:6969/users/logout').then(dados => {
         console.log(dados)
         if (dados.status == 201)
             res.render('index');
@@ -37,20 +37,23 @@ router.get('/register', function (req, res) {
 })*/
 
 router.post('/login', function (req, res) {
-    req.user.dataUltimoAcesso = new Date().toISOString().substr(0,19)
-    //TENHO QUE DAR LOOKUP DO USER, MUDAR A DATA E DEPOIS EDIT, PORQUE O REQ.USER NAO VAI TER A INFO TODA
-    //USAR JWT
-    User.edit(req.user.id, req.user)
-        .then(dados => res.redirect('/'))
-        .catch(err => console.log(err))
+    axios.post('http://localhost:6969/users/login', req.body)
+        .then(dados => {
+            res.cookie('user', dados.data.user, {
+                expires: new Date(Date.now() + '1d'),
+                secure: false, // set to true if your using https
+                httpOnly: true
+            });
+            res.redirect('/')
+        })
+        .catch(erro => res.render(error, { error: erro }))
 })
 
-router.post('/register', function (req, res) {
+/*router.post('/register', function (req, res) {
     req.body.dataRegisto = new Date().toISOString().substr(0,19)
     User.insert(req.body)
         .then(dados => res.redirect('/'))
         .catch(error => res.render('error', { error: error }))
-})
-
+})*/
 
 module.exports = router;
