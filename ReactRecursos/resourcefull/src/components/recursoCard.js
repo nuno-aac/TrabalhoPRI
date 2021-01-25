47708369b1513712b66dace8a4e9acfcf298205f
CoelhoLado <naac.1999@gmail.com>
2021-01-25 17:12:37 +0000

    lista de recursos com sidebar

---
+++ ReactRecursos/resourcefull/src/components/recursoCard.js
@@ -0,0 +1,56 @@
+import '../stylesheets/style.css';
+import '../stylesheets/docstyles.css';
+import '../stylesheets/instyles.css';
+import { Link } from 'react-router-dom'
+
+function timeSince(date) {
+    let now = new Date()
+
+    var seconds = Math.floor((now.getTime() - Date.parse(date)) / 1000);
+    console.log(date + '||-||' + seconds)
+
+    var interval = seconds / 31536000;
+
+    if (interval > 1) {
+        return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+        return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+        return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+        return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
+
+function RecursoCard({recurso}) {
+
+    return (
+        <Link to="/recurso" style={{ textDecoration: 'none' }}>
+            <div className='in-recursos-recurso'>
+                <div className='in-recurso-card'>
+                    <h3>{recurso.titulo.slice(0,14)}{recurso.titulo.length>14 ? '...' : ''}</h3>
+                </div>
+                <div className='in-recurso-card'>
+                    <img src='/images/file.svg' alt='File' className='in-img-card'/>
+                    by {recurso.autor}
+                </div>
+                <div className='in-recurso-card'>
+                    {timeSince(recurso.dataRegisto)} ago
+                </div>
+            </div>
+        </Link>
+    );
+}
+
+export default RecursoCard;
--- ReactRecursos/resourcefull/src/components/recursos.js
+++ ReactRecursos/resourcefull/src/components/recursos.js
@@ -4,6 +4,7 @@ import '../stylesheets/instyles.css';
 import NavbarWrapper from './navbarWrapper';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import RecursoCard from './recursoCard'
 
 function Recursos() {
     let [isLoading,setIsLoading] = useState(true);
@@ -13,7 +14,7 @@ function Recursos() {
         axios.get('http://localhost:6969/recursos', { withCredentials:true })
         .then(dados => { 
             console.log(dados.data)
-            setRecursos(dados.data.recursosPublic) 
+            setRecursos(dados.data.recursosPriv.concat(dados.data.recursosPublic)) 
             setIsLoading(false)
         })
         .catch(err => { console.log(err) })
@@ -21,8 +22,13 @@ function Recursos() {
 
     return (
         <NavbarWrapper>
-            <div className='in-recursos-container'>
-                {isLoading ? <></> : recursos.map((v, i) => <div className='in-recurso' key={i}><div>{v.titulo}</div></div>) }
+            <div className='in-recursos'>
+                <div className='in-recursos-sidebar'>
+
+                </div>
+                <div className='in-recursos-container'>
+                    {isLoading ? <></> : recursos.map((v, i) => <RecursoCard recurso={v} key={i}/>) }
+                </div>
             </div>
         </NavbarWrapper>
     );
